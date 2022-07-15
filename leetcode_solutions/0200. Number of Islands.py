7fb04349bc7411d6c1aa59478f33c3268f2e68be
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-07-15 16:00:34 -0500

    first attempt 0200

--- leetcode_solutions/0200. Number of Islands.py
+++ leetcode_solutions/0200. Number of Islands.py
@@ -7,7 +7,6 @@ Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0'
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume
 all four edges of the grid are all surrounded by water.
 
-
 Example 1:
     Input: grid = [
       ['1','1','1','1','0'],
@@ -33,10 +32,67 @@ Constraints:
     grid[i][j] is '0' or '1'.
 """
 
+import test_cases
+
 
 class Solution:
+    # first attempt
     def numIslands(self, grid: list[list[str]]) -> int:  # noqa
-        pass
+        # island defined as land surrounded by water
+        #   1 with 0s NSEW
+        # DFS grid traversal
+
+        count = 0
+        explored_nodes = []
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                seed_node = (i, j)
+                if grid[seed_node[0]][seed_node[1]] == '0':
+                    explored_nodes.append(seed_node)
+                    continue
+                if seed_node not in explored_nodes:
+                    found_nodes = [seed_node]  # seed
+                    island_nodes = []
+
+                    while found_nodes:
+                        inspected_node = found_nodes.pop(-1)
+                        if grid[inspected_node[0]][inspected_node[1]] == '1':
+                            island_nodes.append(inspected_node)
+
+                            new_nodes = self.find_nodes(grid, inspected_node)
+                            # found_nodes += [each_node for each_node in new_nodes if each_node not in explored_nodes]
+                            for each_node in new_nodes:
+                                if each_node not in explored_nodes:
+                                    found_nodes.append(each_node)
+
+                        explored_nodes.append(inspected_node)
+
+                    if len(island_nodes) > 0:
+                        count += 1
+                        print(count)
+
+        return count
+
+    # find boundary-checked nodes
+    @staticmethod
+    def find_nodes(grid: list[list], element: tuple[int, int], ) -> list[tuple[int, int]]:
+        discovered_nodes = []
+        # cardinal directions: NSEW
+        north_node = (element[0] - 1, element[1])
+        if north_node[0] >= 0:
+            discovered_nodes.append(north_node)
+        south_node = (element[0] + 1, element[1])
+        if south_node[0] < len(grid):
+            discovered_nodes.append(south_node)
+        east_node = (element[0], element[1] + 1)
+        if east_node[1] < len(grid[0]):
+            discovered_nodes.append(east_node)
+        west_node = (element[0], element[1] - 1)
+        if west_node[1] >= 0:
+            discovered_nodes.append(west_node)
+
+        return discovered_nodes
 
 
 class Test(Solution):
@@ -67,8 +123,12 @@ class Test(Solution):
     example_test_2_output = 3
     tests.append((example_test_2, example_test_2_output))
 
-    def test_all(self):
+    tests.append((test_cases.failed_test_0, 0))
+
+    def test_all(self, include: list[int] | None = None):
         for each_test in self.tests:
+            if include is not None and self.tests.index(each_test) not in include:
+                continue
             output = self.numIslands(each_test[0])
             expected_output = each_test[1]
             try:
@@ -84,4 +144,7 @@ class Test(Solution):
                       f"\t\t test_out: {output}\n")
 
 
-Test().test_all()
+Test().test_all(include=[2])
+
+# foo = Test().discover_nodes(Test().tests[0][0], (1, 1))
+# print(foo)
