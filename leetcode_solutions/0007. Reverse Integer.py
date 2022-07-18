90256b9c3a2e2a94009fb81d65dde5757e778bd4
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-07-18 16:38:07 -0500

    solved for correctness and imposed constraints; poor quality

--- leetcode_solutions/0007. Reverse Integer.py
+++ leetcode_solutions/0007. Reverse Integer.py
@@ -25,9 +25,35 @@ Constraints:
 
 
 class Solution:
+    # second attempt: referenced mostly useless approaches
+    # poor approach to solution (using strings); not optimized
+    def reverse(self, x: int) -> int:  # noqa
+        # if same order of magnitude (10)
+        # read right to left
+        #   compare to boundary value
+        #     if greater, return 0
+        #     if equal, continue
+        #     if less, return number
+
+        reversed_signed_int = str(abs(x))[::-1]
+        max_val = 2 ** 31 - 1
+        oom = len(str(max_val))  # order of magnitude
+        if len(reversed_signed_int) < oom:
+            return int(reversed_signed_int) if x > 0 else -int(reversed_signed_int)
+        elif len(reversed_signed_int) == oom:
+            for i, each_digit in enumerate(reversed_signed_int):
+                if i == len(reversed_signed_int) or each_digit < str(max_val)[i]:
+                    return int(reversed_signed_int) if x > 0 else -int(reversed_signed_int)
+                # can ignore +- differences due to input constraint
+                if each_digit == str(max_val)[i]:
+                    continue
+                if each_digit > str(max_val)[i]:
+                    break
+        return 0
+
     # solve for correctness
     # incorrect solution: environment does not 64-bit int storage
-    def reverse(self, x: int) -> int:  # noqa
+    def first_attempt_reverse(self, x: int) -> int:  # noqa
         sign = 1
         if x < 1:
             sign = -1
@@ -58,6 +84,11 @@ class Test(Solution):
         9_463_841_472:
             0,
 
+        2_147_447_412:  # largest number without overflow
+            2_147_447_412,
+
+        1_534_236_469:
+            0,
 
     }
 
