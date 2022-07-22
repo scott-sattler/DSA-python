5c975d2edc30d6abd1b8c97dd2c8ab9770601cfb
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-07-22 14:06:24 -0500

    neatness

--- leetcode_solutions/1480. Running Sum of 1d Array.py
+++ leetcode_solutions/1480. Running Sum of 1d Array.py
@@ -29,7 +29,7 @@ Constraints:
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]: # noqa
         for i in range(1, len(nums)):
-            nums[i] = nums[i - 1] + nums[i]
+            nums[i] += nums[i - 1]
         return nums
 
 
