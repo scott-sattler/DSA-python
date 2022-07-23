49e43351eee6acdf1e6ce3f5c0355a0692d04539
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-07-23 17:42:34 -0500

    solution: time 97.39%; mem 20.82%

--- leetcode_solutions/0206. Reverse Linked List.py
+++ leetcode_solutions/0206. Reverse Linked List.py
@@ -23,16 +23,68 @@ Constraints:
 Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 """
 
+from typing import Optional
+import helper_functions as hf
+
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):  # noqa
+        self.val = val
+        self.next = next
+
+
 class Solution:
+    # first attempt
+    # time complexity: O(n)
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:  # noqa
-        pass
+        # create ll, backwards
+
+        reversed_ll: ListNode | None = None
+        linked_list = head
+
+        while linked_list:
+            reversed_ll = ListNode(linked_list.val, reversed_ll)
+            linked_list = linked_list.next
+
+        return reversed_ll
 
 
 class Test(Solution):
-    pass
+    tests = [
+        # provided
+        [
+            [1, 2, 3, 4, 5],
+            [5, 4, 3, 2, 1]
+        ],
+        [
+            [1, 2],
+            [2, 1]
+        ],
+        [
+            [],
+            []
+        ],
+
+    ]
+
+    def test_all(self):
+        pass_count = 0
+        for each_test in self.tests:
+            test_input = each_test[0]
+            expected_output = each_test[1]
+            actual_output = hf.ll_to_non_ll(self.reverseList(hf.create_ll(test_input)))
+            try:
+                assert actual_output == expected_output
+                print("PASS", end="")
+                pass_count += 1
+            except AssertionError:
+                print("FAIL", end="")
+            finally:
+                print(f"  \t test_inp: {test_input}\n"
+                      f"\t\t expd_out: {expected_output}\n"
+                      f"\t\t test_out: {actual_output}\n")
+        print(f'SUMMARY: TESTED {len(self.tests)} | PASSED {pass_count} | FAILED {len(self.tests) - pass_count}')
+
+
+Test().test_all()
