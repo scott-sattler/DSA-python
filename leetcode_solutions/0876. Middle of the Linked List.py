7560f2ae21618e96e1250f0a3d3c4abd34fd1bc8
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-07-25 14:08:29 -0500

    solution: time 99.20%; mem 95.68%

--- leetcode_solutions/0876. Middle of the Linked List.py
+++ leetcode_solutions/0876. Middle of the Linked List.py
@@ -22,6 +22,7 @@ Constraints:
 """
 
 from typing import Optional
+import helper_functions as hf
 
 
 # Definition for singly-linked list.
@@ -32,9 +33,75 @@ class ListNode:
 
 
 class Solution:
+    # first attempt: two pointers
+    # time complexity: O(n)
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:  # noqa
-        pass
+        # traverse ll
+        #   record observed
+        # calculate median
+        # hash lookup
+
+        hash_lookup: dict = {}
+
+        i = 0
+        while head:
+            hash_lookup[i] = head
+
+            head = head.next
+            i += 1
+
+        median_node = i // 2  # len(ll) // 2 -> rightmost on even
+
+        return hash_lookup[median_node]
 
 
 class Test(Solution):
-    pass
+    tests = (
+        # provided
+        (
+            [1, 2, 3, 4, 5],
+            [3, 4, 5]
+        ),
+        (
+            [1, 2, 3, 4, 5, 6],
+            [4, 5, 6]
+        ),
+
+
+        (
+            [3],
+            [3]
+        ),
+        (
+            [4, 3, 2, 1, 5, 3, 7, 8, 100, 99, 98, 45],
+            [7, 8, 100, 99, 98, 45]
+        ),
+        (
+            [4, 3, 2, 1, 5, 3, 7, 8, 100, 99, 98],
+            [3, 7, 8, 100, 99, 98]
+        ),
+
+
+    )
+
+    def test_all(self):
+        pass_count = 0
+        for each_test in self.tests:
+            test_input = each_test[0]
+            expected_output = each_test[1]
+            actual_output = hf.ll_to_non_ll(self.middleNode(hf.create_ll(test_input)))
+            try:
+                assert actual_output == expected_output
+                print("PASS", end="")
+                pass_count += 1
+            except AssertionError:
+                print("FAIL", end="")
+            finally:
+
+                print(f"  \t test_inp: {test_input}\n"
+                      f"\t\t expd_out: {expected_output}\n"
+                      f"\t\t test_out: {actual_output}\n")
+        print(f'SUMMARY: TESTED {len(self.tests)} | PASSED {pass_count} | FAILED {len(self.tests) - pass_count}')
+
+
+Test().test_all()
