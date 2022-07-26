5c2244bc0860f523c71af4efeeb3fb305851c3e7
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-07-26 14:55:44 -0500

    added looping linked list functions; minor other

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from typing import Optional
+import warnings
 
 
 # Definition for singly-linked list.
@@ -10,9 +12,11 @@ class ListNode:
         self.next = next
 
 
-# todo
+# todo deprecate
 def create_linked_list(number: list | tuple) -> ListNode | None:  # 3.11 -> Self
     """ warning: destructively consumes list """
+    warnings.WarningMessage("deprecated: use create_ll")
+
     if not isinstance(number, list):
         number = list(number)
 
@@ -33,6 +37,47 @@ def create_ll(list_of_things: list | tuple) -> ListNode | None:
         node = node.next
         node.val = each_element
     return head.next
+
+
+# creates looping linked list from the last node to the specified node
+def create_looped_ll(list_of_things: list | tuple, loops_at: int = -1) -> ListNode | None:
+    loop_reference: ListNode | None = None
+    node = ListNode()
+    head = node
+    for i, each_element in enumerate(list_of_things):
+        node.next = ListNode()
+        node = node.next
+        node.val = each_element
+        if -1 < loops_at == i:
+            loop_reference = node
+    else:
+        node.next = loop_reference
+
+    return head.next
+
+
+def get_depth_of_ll_node(linked_list: ListNode, find_this_node: ListNode) -> int | None:
+    """ Given a node, find its index (or depth) within the provided linked list """
+    index = 0
+    node = linked_list
+    while node:
+        if node == find_this_node:
+            return index
+        node = node.next
+        index += 1
+    return None
+
+
+def get_ll_node_at_depth(linked_list: ListNode, node_depth: int) -> ListNode | None:
+    """ Given a node depth, return the node at the given depth """
+    index = 0
+    node = linked_list
+    while node:
+        if index == node_depth:
+            return node
+        node = node.next
+        index += 1
+    return None
 
 
 def print_linked_list(linked_list: ListNode) -> None:
