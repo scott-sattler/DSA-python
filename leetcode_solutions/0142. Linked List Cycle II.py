4d4c278ea875f3de86b78ead35a8f495ca1b516e
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-07-26 16:28:49 -0500

    added follow-up: space complexity O(1)

--- leetcode_solutions/0142. Linked List Cycle II.py
+++ leetcode_solutions/0142. Linked List Cycle II.py
@@ -29,6 +29,13 @@ Constraints:
     pos is -1 or a valid index in the linked-list.
 
 Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+Note:   mutable, user defined objects are hashable in Python
+        uses their id()
+        unclear how to use id() to access object, like c pointer
+
+        https://stackoverflow.com/a/56012972
+        https://stackoverflow.com/a/66501227
 """
 
 from typing import Optional
@@ -43,8 +50,62 @@ class ListNode:
 
 
 class Solution:
+    # follow-up attempt
+    # time complexity: O(n)
+    # space complexity: O(1)
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:  # noqa
-        pass
+        # modifying first attempt approach
+        # change the .val type to indicate visited status
+        #   here, the int is put into a list
+        # removing val changes would be O(n^2) time
+
+        node = head
+        while node:
+            if isinstance(node.val, list):
+                node.val = node.val[0]
+                return node
+            else:  # not visited
+                node.val = [node.val]
+                node = node.next
+
+        return None
+
+    # first attempt
+    # time complexity: O(n)
+    # space complexity: O(n)?
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:  # noqa
+        # traverse ll
+        #   check if visited
+        #   record observed nodes
+
+        node = head
+        observed: dict[ListNode, any] = {}
+        while node:
+            observed_node: str | None = observed.get(node, None)
+            if observed_node is not None:
+                return node
+            else:  # not in observed dict
+                observed[node] = ''
+                node = node.next
+
+        return None
+
+    # second attempt: id variation
+    def second_attempt_detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:  # noqa
+        # traverse ll
+        #   check if visited
+        #   record observed nodes
+
+        node = head
+        observed: dict[id(ListNode), ListNode] = {}
+        while node:
+            observed_node: ListNode | None = observed.get(id(node), None)
+            if observed_node is not None:
+                return observed_node
+            else:  # not in observed dict
+                observed[id(node)] = node
+                node = node.next
+        return None
 
 
 class Test(Solution):
@@ -63,6 +124,33 @@ class Test(Solution):
             None
         ),
 
+
+        (
+            [[-1, 3, 1, 5, 1], -1],
+            None
+        ),
+        (
+            [[0, 1, 2, 3, 4], 4],
+            4
+        ),
+        (
+            [[0, 1, 2, 3, 4], 0],
+            0
+        ),
+        (
+            [[0, 1, 2, 3, 4], 1],
+            1
+        ),
+        (
+            [[-6, -3, -1, 0, 1], 4],
+            4
+        ),
+
+        (
+            [[1, 2], 0],
+            0
+        ),
+
     )
 
     def test_all(self):
@@ -75,7 +163,7 @@ class Test(Solution):
             # test <-> solution format conversions: list[int]/int/None <-> ListNode
             looped_ll = hf.create_looped_ll(test_input, hidden_input)
             actual_output = self.detectCycle(looped_ll)
-            actual_output = hf.get_depth_of_ll_node(looped_ll, actual_output)
+            actual_output = hf.get_depth_of_ll_node(looped_ll, actual_output)  # convert to testing format
             try:
                 assert actual_output == expected_output
                 print("PASS", end="")
