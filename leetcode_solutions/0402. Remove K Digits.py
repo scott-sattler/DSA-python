4c0c8641da5edb468fd489e0218d8e235913e160
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-12-12 21:57:29 -0600

    initial commit; testing w/o test cases

---
+++ leetcode_solutions/0402. Remove K Digits.py
@@ -0,0 +1,88 @@
+"""
+402. Remove K Digits
+Medium
+
+Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after
+removing k digits from num.
+
+Example 1:
+    Input: num = "1432219", k = 3
+    Output: "1219"
+    Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+
+Example 2:
+    Input: num = "10200", k = 1
+    Output: "200"
+    Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+
+Example 3:
+    Input: num = "10", k = 2
+    Output: "0"
+    Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+Constraints:
+    1 <= k <= num.length <= 10^5
+    num consists of only digits.
+    num does not have any leading zeros except for the zero itself.
+
+"""
+
+
+class Solution:
+    # first attempt
+
+    def removeKdigits(self, num: str, k: int) -> str:  # noqa: naming convention
+        return ""
+
+
+# taken from 0055
+class Test:
+    # format: [ ( input , expected_out ), ]
+    test_cases: list[tuple[list, bool]] = [
+        # provided
+        (("1432219", 3), "1219"),
+        (("10200", 1), "200"),
+        (("10", 2), "0"),
+
+        # additional
+
+
+    ]
+
+    def test_all(self, *include):
+        """
+        :param include: eg .test_all(0, 2, 5); or .test_all()
+        :return: None
+        """
+        tests = self.test_cases
+        s = Solution()
+
+        pass_count = 0
+        tests_failed = []
+        for i, each_test in enumerate(tests):
+            if len(include) > 0 and i not in include:
+                continue
+            test_input = each_test[0]
+            expected_output = each_test[1]
+            actual_output = s.canJump(test_input)
+            try:
+                assert actual_output == expected_output
+                print("PASS", end="")
+                pass_count += 1
+            except AssertionError:
+                print("FAIL", end="")
+                tests_failed.append(i)
+            finally:
+
+                print(f"  \t test_{i:03d}: {each_test}\n"
+                      f"\t\t expd_out: {expected_output}\n"
+                      f"\t\t test_out: {actual_output}\n")
+
+        tested = len(include) if len(include) > 0 else len(tests)
+        print(f'SUMMARY: TESTED {tested} | PASSED {pass_count} | FAILED {len(tests_failed)}')
+        print(f'FAILED TESTS: {[f"test_{i:03d}" for i in tests_failed]}') if len(tests_failed) > 0 else print(end='')
+
+
+# example usages
+# Test().test_all(0, 2, 5); Test().test_all()
+Test().test_all()
