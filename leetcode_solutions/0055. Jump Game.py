0c779d3e94169db657b8518c352ad71398106d08
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-12-12 20:17:13 -0600

    unsolved; imported testing from 0299; modified w/ improvements

--- leetcode_solutions/0055. Jump Game.py
+++ leetcode_solutions/0055. Jump Game.py
@@ -27,3 +27,46 @@ Constraints:
 class Solution:
     def canJump(self, nums: list[int]) -> bool:  # noqa: naming convention
         return
+
+
+# taken from 0299; modified
+class Test:
+    # format:
+    test_cases: list[tuple[list, bool]] = [
+        # provided
+        ([2, 3, 1, 1, 4], True),
+        ([3, 2, 1, 0, 4], False),
+
+    ]
+
+    def test_all(self, *include):
+        tests = self.test_cases
+        s = Solution()
+
+        pass_count = 0
+        tests_failed = []
+        for i, each_test in enumerate(tests):
+            if len(include) > 0 and i not in include:
+                continue
+            test_input = each_test[0]
+            expected_output = each_test[1]
+            actual_output = s.canJump(test_input)
+            try:
+                assert actual_output == expected_output
+                print("PASS", end="")
+                pass_count += 1
+            except AssertionError:
+                print("FAIL", end="")
+                tests_failed.append(i)
+            finally:
+
+                print(f"  \t test_{i:03d}: {each_test}\n"
+                      f"\t\t expd_out: {expected_output}\n"
+                      f"\t\t test_out: {actual_output}\n")
+
+        tested = len(include) if len(include) > 0 else len(tests)
+        print(f'SUMMARY: TESTED {tested} | PASSED {pass_count} | FAILED {len(tests_failed)}')
+        print(f'FAILED TESTS: {[f"test_{i:03d}" for i in tests_failed]}') if len(tests_failed) > 0 else print(end='')
+
+
+Test().test_all()
