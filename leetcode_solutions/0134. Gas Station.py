ca6d7d84344cf053e973b66d70429674c537936f
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-12-14 11:15:48 -0600

    initial commit; template; testing framework 0402; only provided test cases

---
+++ leetcode_solutions/0134. Gas Station.py
@@ -0,0 +1,99 @@
+"""
+134. Gas Station
+Medium
+
+There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next
+(i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once
+in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+Example 1:
+    Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+    Output: 3
+    Explanation:
+        Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+        Travel to station 4. Your tank = 4 - 1 + 5 = 8
+        Travel to station 0. Your tank = 8 - 2 + 1 = 7
+        Travel to station 1. Your tank = 7 - 3 + 2 = 6
+        Travel to station 2. Your tank = 6 - 4 + 3 = 5
+        Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+        Therefore, return 3 as the starting index.
+
+Example 2:
+    Input: gas = [2,3,4], cost = [3,4,3]
+    Output: -1
+    Explanation:
+        You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+        Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+        Travel to station 0. Your tank = 4 - 3 + 2 = 3
+        Travel to station 1. Your tank = 3 - 3 + 3 = 3
+        You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+        Therefore, you can't travel around the circuit once no matter where you start.
+
+Constraints:
+    n == gas.length == cost.length
+    1 <= n <= 10^5
+    0 <= gas[i], cost[i] <= 10^4
+
+"""
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:  # noqa: naming convention
+        pass
+
+
+# taken from 0402
+class Test:
+    # preprocessing
+
+    # format: [ ( input , expected_out ), ]
+    test_cases: list[tuple[list, list, int]] = [
+        # provided
+        ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3),
+        ([2, 3, 4], [3, 4, 3], -1),
+
+        # additional
+
+    ]
+
+    def test_all(self, *include):
+        """
+        :param include: eg .test_all(0, 2, 5); or .test_all()
+        :return: None
+        """
+        tests = self.test_cases
+        s = Solution()
+
+        pass_count = 0
+        tests_failed = []
+        for i, each_test in enumerate(tests):
+            if len(include) > 0 and i not in include:
+                continue
+            test_input = each_test[0]
+            expected_output = each_test[1]
+            actual_output = s.canCompleteCircuit(test_input[0], test_input[1])  # <--- fn name here
+            try:
+                assert actual_output == expected_output
+                print("PASS", end="")
+                pass_count += 1
+            except AssertionError:
+                print("FAIL", end="")
+                tests_failed.append(i)
+            finally:
+
+                print(f"  \t test_{i:03d}: {each_test}\n"
+                      f"\t\t expd_out: {expected_output}\n"
+                      f"\t\t test_out: {actual_output}\n")
+
+        tested = len(include) if len(include) > 0 else len(tests)
+        print(f'SUMMARY: TESTED {tested} | PASSED {pass_count} | FAILED {len(tests_failed)}')
+        print(f'FAILED TESTS: {[f"test_{i:03d}" for i in tests_failed]}') if len(tests_failed) > 0 else print(end='')
+
+
+# example usages
+# Test().test_all(0, 2, 5); Test().test_all()
+Test().test_all()
