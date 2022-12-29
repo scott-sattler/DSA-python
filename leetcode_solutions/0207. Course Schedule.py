13a7e1e01f5b77beed635bd86e36926d94071f75
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-12-28 19:37:23 -0600

    initial commit; testing w/o test cases

---
+++ leetcode_solutions/0207. Course Schedule.py
@@ -0,0 +1,109 @@
+"""
+207. Course Schedule
+Medium
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array
+prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course
+ai.
+
+    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+
+Return true if you can finish all courses. Otherwise, return false.
+
+Example 1:
+    Input: numCourses = 2, prerequisites = [[1,0]]
+    Output: true
+    Explanation: There are a total of 2 courses to take.
+    To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+    Output: false
+    Explanation: There are a total of 2 courses to take.
+    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So
+    it is impossible.
+
+Constraints:
+    1 <= numCourses <= 2000
+    0 <= prerequisites.length <= 5000
+    prerequisites[i].length == 2
+    0 <= ai, bi < numCourses
+    All the pairs prerequisites[i] are unique.
+
+"""
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:  # noqa: naming convention
+        pass
+
+
+# taken from 0841
+class TestCase:
+    def __init__(self, num_courses: int, prerequisites: list[list[int]], output: bool) -> None:
+        self.num_courses = num_courses
+        self.prerequisites = prerequisites
+        self.output = output
+
+    def __repr__(self):
+        return f"{self.num_courses} {self.prerequisites} {self.output}"
+
+
+# taken from 0841
+class Test:
+    # preprocessing (boundaries)
+    # Not Implemented
+
+    test_cases: list[TestCase] = [
+        # provided
+        TestCase(2, [[1, 0]], True),
+        TestCase(2, [[1, 0], [0, 1]], False),
+
+        # failed
+
+    ]
+
+    def test_all(self, *include):
+        """
+        :param include: eg .test_all(0, 2, 5); or .test_all()
+        :return: None
+        """
+        tests = self.test_cases
+        s = Solution()
+
+        pass_count = 0
+        tests_failed = []
+        for i, each_test in enumerate(tests):
+            if len(include) > 0 and i not in include:
+                continue
+            test_input_01 = each_test.num_courses  # <--- modify as needed
+            test_input_02 = each_test.prerequisites  # <--- modify as needed
+            expected_output = each_test.output  # <--- modify as needed
+            actual_output = s.canFinish(test_input_01, test_input_02)  # <--- fn name here
+            try:
+                assert actual_output == expected_output
+                print("PASS", end="")
+                pass_count += 1
+            except AssertionError:
+                print("FAIL", end="")
+                tests_failed.append(i)
+            finally:
+                print(f"  \t test_{i:03d}: {each_test}\n"
+                      f"\t\t expd_out: {expected_output}\n"
+                      f"\t\t test_out: {actual_output}\n")
+
+        tested = len(include) if len(include) > 0 else len(tests)
+        print(f'SUMMARY: TESTED {tested} | PASSED {pass_count} | FAILED {len(tests_failed)}')
+        print(f'FAILED TESTS: {[f"test_{i:03d}" for i in tests_failed]}') if len(tests_failed) > 0 else print(end='')
+
+
+# example usages
+# Test().test_all(0, 2, 5); Test().test_all()
+Test().test_all()
+
+
+
+
+
+
+
