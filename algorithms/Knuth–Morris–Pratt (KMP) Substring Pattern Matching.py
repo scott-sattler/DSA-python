f2c2945ab500c057adc97994e66428006c15756c
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-06-16 16:57:56 -0500

    added testing automation

--- algorithms/Knuth–Morris–Pratt (KMP) Substring Pattern Matching.py
+++ algorithms/Knuth–Morris–Pratt (KMP) Substring Pattern Matching.py
@@ -1,10 +1,10 @@
 # naive
-def brute_force(pattern: str, string: str):
+def brute_force(pattern: str, text: str):
     matches = list()
     p_i, s_i = 0, 0
-    while s_i < len(string):
+    while s_i < len(text):
         # individual char match
-        if pattern[p_i] != string[s_i]:
+        if pattern[p_i] != text[s_i]:
             s_i = (s_i - p_i) + 1  # backtrack (reset), increment
             p_i = 0
         # full pattern match
@@ -20,7 +20,27 @@ def brute_force(pattern: str, string: str):
     return matches
 
 
+# prefix length array
+# text = 'abcabcg'
+#       'a  b  c  a  b  c  g'
+# pla = [0, 0, 0, 1, 2, 3, 0]
+
+# def kmp(text: str, pattern: str):
+#     # prefix length array
+#     pla = list()
+#     n = len(pattern)
+#     length = 0
+#     for i in range(n):
+#         if i + 1 ==
+
+
+
+
+
+
+
 test_cases = [
+    # basic naive tests
     ('abc', 'abc', [0]),
     ('abc', 'abcd', [0]),
     ('dabc', 'abc', []),
@@ -33,18 +53,20 @@ test_cases = [
     ('baaac', 'aaaaaa', []),
     ('aaa', 'baaaaaac', [1, 2, 3, 4]),
 
+    # kmp specific tests
+    ('abcabcg', 'aaabcabcabcxabcabcg', [12]),
+
 ]
 
-for each_test in test_cases:
-    print(each_test, end=' ')
-    try:
-        assert brute_force(each_test[0], each_test[1]) == each_test[2]
-        print('PASS')
-    except AssertionError:
-        print('FAIL', end=' ')
-        print(brute_force(each_test[0], each_test[1]))
-        # assert brute_force(each_test[0], each_test[1]) == each_test[2]  # for easy debugging
 
-
-
-
+test_fns = {name: obj for name, obj in globals().copy().items() if callable(obj)}
+for each_fn in test_fns[1:]:
+    for each_test in test_cases:
+        print(each_test, end=' ')
+        try:
+            assert each_fn(each_test[0], each_test[1]) == each_test[2]
+            print('PASS')
+        except AssertionError:
+            print('FAIL', end=' ')
+            print(each_fn(each_test[0], each_test[1]))
+            # assert brute_force(each_test[0], each_test[1]) == each_test[2]  # for easy debugging
