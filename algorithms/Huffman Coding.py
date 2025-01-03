67c72cf554a7c48b8f8b59a470e63854c88de9d2
Scott Sattler <scott.sattler.dev@gmail.com>
2025-01-03 17:23:14 -0600

    initial commit

--- algorithms/Huffman Coding.py
+++ algorithms/Huffman Coding.py
@@ -1,12 +1,20 @@
-# noinspection SpellCheckingInspection
-inp = "A DEAD DAD CEDED A BAD BABE A BEADED ABACA BED"
-
-exp_out = "100001"
-
-
-def huffman_encode():
-    ...
+class Huffman:
+    def __init__(self):
+        ...
 
 
-def huffman_decode():
-    ...
+    def encode(self, unencoded):
+        ...
+
+
+    def decode(self, encoded):
+        ...
+
+
+if __name__ == '__main__':
+    # noinspection SpellCheckingInspection
+    inp_str = "A DEAD DAD CED"
+
+    exp_out = [b'00', b'01', b'11', b'101', b'00', b'11', b'01', b'11', b'00', b'11', b'01', b'100', b'101', b'11']
+
+    actual_out = Huffman().encode(inp_str)
