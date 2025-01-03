946b7c8dadc808da2b3252409ac68ffdcb48a3d4
Scott Sattler <scott.sattler.dev@gmail.com>
2025-01-03 17:09:40 -0600

    initial commit

---
+++ algorithms/Huffman Coding.py
@@ -0,0 +1,12 @@
+# noinspection SpellCheckingInspection
+inp = "A DEAD DAD CEDED A BAD BABE A BEADED ABACA BED"
+
+exp_out = "100001"
+
+
+def huffman_encode():
+    ...
+
+
+def huffman_decode():
+    ...
