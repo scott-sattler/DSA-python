2b41e79ede0241ab22256bfbe70ea49b3b4ac9df
Scott Sattler <scott.sattler.dev@gmail.com>
2025-03-16 23:09:48 -0500

    removed notes

--- algorithms/binary_rotated_array_search.py
+++ algorithms/binary_rotated_array_search.py
@@ -62,31 +62,6 @@ def remove_rotation(array: list, zero_index):
             j += 1
     return array
 
-'''
-4 5 1 2 3
-^   ^
-1 5 4 2 3
-  ^   ^
-1 2 4 5 3
-    ^   ^
-1 2 3 5 4
-      ^ ^
-1 2 3 4 5
-
-
-6 1 2 3 4 5
-^ ^
-1 6 2 3 4 5
-  ^ ^
-  
-2 3 4 5 1
-^       ^
-1 3 4 5 2
-  ^     ^
-1 2 4 5 3
-
-
-'''
 
 import pytest
 
