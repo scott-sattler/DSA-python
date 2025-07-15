a57c12c8474d7ffe0fae2ffaa7500f70f3f4ddb1
Scott Sattler <scott.sattler.dev@gmail.com>
2025-07-15 00:40:11 -0500

    practice. todo variant

---
+++ leetcode_solutions/practice/1290. Convert Binary Number in a Linked List to Integer.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    # def getDecimalValue(self, head: Optional[ListNode]) -> int:
+    #     bin_val = []
+    #     while head is not None:
+    #         bin_val.append(str(head.val))
+    #         head = head.next
+    #     return int(''.join(bin_val), 2)
+
+    # todo: must use O(1) auxiliary space
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        ...
+
+
+
+
+
